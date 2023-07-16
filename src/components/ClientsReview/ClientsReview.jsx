e8442478b167e04e1a9039d48505da5e027e581a
Kiran <kiran@kali.kali>
2023-07-17 01:05:53 +0530

    switch check-form fix

--- src/components/ClientsReview/ClientsReview.jsx
+++ src/components/ClientsReview/ClientsReview.jsx
@@ -7,20 +7,20 @@ const ClientsReview = () => {
     const [isChecked, setIsChecked] = useState(false);
     return (
         <>
-            <div className="conatainer d-flex justify-content-center text-center">
-                <div class="form-check form-switch ">
+            <div className="conatainer d-flex justify-content-center text-center m-3 m-md-4 m-lg-5" data-aos="fade-right">
+                <div class="form-check form-switch"  >
                     <input class="form-check-input review-switcher" type="checkbox" id="formSwitchDefault"
-                   
-
+                        style={{ width: '70px', height: '28px', margin: "0 3rem 0 0" }}
                         checked={isChecked}
                         onChange={() => {
                             setIsChecked((prevstate) => (!prevstate))
                         }} />
-                    <label class="form-check-label" for="formSwitchDefault"><h3>{isChecked ? "Dynamic reviews" : "Static reviews"}</h3></label>
+                    <label class="form-check-label" for="formSwitchDefault"><h4>{isChecked ? "Dynamic reviews" : "Static reviews"}</h4></label>
                 </div>
-            </div>
+            </div >
 
-            {isChecked ? <ClientsReviewStatic /> : <ClientsReviewDynamic />}
+            {isChecked ? <ClientsReviewDynamic /> : <ClientsReviewStatic />
+            }
 
         </>
     )
