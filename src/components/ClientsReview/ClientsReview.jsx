fffa93c064d454df6d24220264d874a5d76bca0a
Kiran <kiran@kali.kali>
2023-07-17 00:53:27 +0530

    add switch for review section change

--- src/components/ClientsReview/ClientsReview.jsx
+++ src/components/ClientsReview/ClientsReview.jsx
@@ -1,12 +1,26 @@
 import "./ClientsReview.css"
 import ClientsReviewStatic from "./ClientsReviewStatic"
 import ClientsReviewDynamic from "./ClientsReviewDynamic"
+import { useState } from "react"
 
 const ClientsReview = () => {
+    const [isChecked, setIsChecked] = useState(false);
     return (
         <>
-            <ClientsReviewStatic />
-            <ClientsReviewDynamic />
+            <div className="conatainer d-flex justify-content-center text-center">
+                <div class="form-check form-switch ">
+                    <input class="form-check-input review-switcher" type="checkbox" id="formSwitchDefault"
+                   
+
+                        checked={isChecked}
+                        onChange={() => {
+                            setIsChecked((prevstate) => (!prevstate))
+                        }} />
+                    <label class="form-check-label" for="formSwitchDefault"><h3>{isChecked ? "Dynamic reviews" : "Static reviews"}</h3></label>
+                </div>
+            </div>
+
+            {isChecked ? <ClientsReviewStatic /> : <ClientsReviewDynamic />}
 
         </>
     )
